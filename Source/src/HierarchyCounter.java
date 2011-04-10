3bb8be0d88c291c54ce8e54c1f6ff534ed530e85
csomakk <csomakk@gmail.com>
2011-04-10 23:23:02 +0200

    feedback és compositok még nem műkszenek, de az egyszerű áramkör faszásan lefordul :D
    Signed-off-by: csomakk <csomakk@gmail.com>

--- Source/src/HierarchyCounter.java
+++ Source/src/HierarchyCounter.java
@@ -3,15 +3,40 @@ import java.util.*;	// List, ArrayList-hez
 //committok miatt külön class, végén majd bele lehet rakni egybe, de igy nem nyúlok keresztbe nektek
 
 public class HierarchyCounter {
+	List<DigitalObject> sources;
+	List<DigitalObject> list;
+	List<Wire> wires;
+	ArrayList<List<DigitalObject>> components;
 	
-	public void CountHierarchy(List<Wire> wires, ArrayList<List<DigitalObject>> components){
-		List<DigitalObject> sources=new ArrayList<DigitalObject>(); //forrásokat különvesszük
-		List<DigitalObject> list=new ArrayList<DigitalObject>(); //bejáratlan
+	public int getSzint(DigitalObject ennek){
+		int ret=-1;
+		for(int i=0; i<components.size(); i++){
+			if(-1!=components.get(i).indexOf(ennek));
+		}
+		return ret;
+	}
+	
+	public void moveToSzint(DigitalObject ezt, int ide){
+		if(components.size()<ide) components.add(new ArrayList<DigitalObject>()); //ha nincs még szint+1 hierarchia
+		if(getSzint(ezt)<0) list.remove(ezt); //ha nincs a hierarchiában, a bejáratlanból vesszük ki
+		if(components.get(getSzint(ezt)).indexOf(ezt)!=-1) components.get(getSzint(ezt)).remove(ezt); //ha a listában van, kivesszük
+		components.get(ide).add(ezt);//hozzáadjuk a kért hierarchiára
+	}
+	
+	public void foo(){}
+	
+	public void CountHierarchy(List<Wire> wiresArg, ArrayList<List<DigitalObject>> componentsArg){
+		sources=new ArrayList<DigitalObject>(); //forrásokat különvesszük
+		list=new ArrayList<DigitalObject>(); //bejáratlan
 		list.clear();
-		//componentnél más a source!! ez todo
+		wires=wiresArg;
+		components=componentsArg;
+				
+		//componentnél más a source tipusa!! ez TODO
 		for(int i=0 ; i<components.size(); i++){
 			for(int j=0; j<components.get(i).size();j++){
 				String classs=components.get(i).get(j).getClass().toString();
+				//hozzáadjuk a sourceokhoz vagy a bejáratlanokhoz
 				if(classs.equals("class SWITCH") || classs.equals("class GENERATOR")) sources.add(components.get(i).get(j));
 				else list.add(components.get(i).get(j));
 			}
@@ -30,23 +55,26 @@ public class HierarchyCounter {
 			for(int i=0; i<components.get(szint).size(); i++){ //minden forrás
 				for(int j=0; j<components.get(szint).get(i).wireOut.size();j++){ //minden kimenet wireének
 					for(int k=0; k<components.get(szint).get(i).wireOut.get(j).objectsOut.size();k++){ //minden kimenete
-						if(-1==list.indexOf(components.get(szint).get(i).wireOut.get(j).objectsOut.get(k))){
+						DigitalObject current=components.get(szint).get(i).wireOut.get(j).objectsOut.get(k);
+						if(-1==list.indexOf(current)){
 							//ha már volt, 
 							//a) feedback
 							//b) hierarchia nõhet
+							if(getSzint(current)<szint+1) foo(); //feed back lesz TODO
+							if(getSzint(current)>=szint+1) moveToSzint(current, szint+1); //nõ a szintje
 							
 						} else {
 							//ha még nem volt
-							if(components.size()<szint+1) components.add(new ArrayList<DigitalObject>()); //ha nincs még szint+1 hierarchia
-							components.get(szint+1).add(components.get(szint).get(i).wireOut.get(j).objectsOut.get(k));//hozzáadjuk a köv. hierarchiára
-							list.remove(components.get(szint).get(i).wireOut.get(j).objectsOut.get(k)); //és levesszük a bejáratlan listáról
+							moveToSzint(components.get(szint).get(i).wireOut.get(j).objectsOut.get(k), szint+1);
 						}
 					}
 				}
 			}
 			
 			szint++;
-		}
+		}//while list.size>0
 		
-	}
-}
+		componentsArg=components;
+		
+	}//countHierarchy
+}//class
