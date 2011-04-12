45ef2fc89cf4ebc0d11179187958edd59ba8de8d
seymor_linux <seymor@seymor.hu>
2011-04-12 10:33:17 +0200

    Peti forrasai

---
+++ Peti/Main.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bhdlparser;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Daneee
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // A fájlunk betöltve
+       String file="composit main (in, out)\nled led1;\ngenerator generator1;\nswitch switch1;\nled1 =  generator1 | switch1;\nendcompositcomposit mn (in, out)\nled led1;\ngenerator generator1;\nswitch switch1;\nled1 =  generator1 | switch1;\nendcomposit";
+       ArrayList<String> foundComposites = new ArrayList<String>();
+
+       System.out.println(file);
+       System.out.println();
+
+       //Kivesszük entereket
+       file = RegularParser.remove_CR_LF(file);
+       System.out.println(file);
+
+
+       //Amig nem üres a file
+
+       while(!file.equals("")){
+
+           //Megkeressük composite-t
+       String composit = RegularParser.find_next_Composite(file);
+       System.out.println(composit);
+       System.out.println();
+
+       //Kivesszük régiből
+       file = file.replace(composit,"");
+       System.out.println(file);
+       System.out.println();
+
+       //Hozzáadjuk a tömbhöz
+       foundComposites.add(composit);
+       }
+
+       for(String found :foundComposites){
+            System.out.println(found);
+            System.out.println();
+       }
+    }
+}
