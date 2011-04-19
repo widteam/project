be0d20f34b80faa87fa58964975cafc4699f6a5e
Gyuri <sziklaygy@gmail.com>
2011-04-19 21:17:51 +0200

    minden

--- Gyuri_kedd_hajnal/src/Main.java
+++ Gyuri_kedd_hajnal/src/Main.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class main {
 
 	public static void main(String[] args) {
 		InputStreamReader input = new InputStreamReader(System.in);
