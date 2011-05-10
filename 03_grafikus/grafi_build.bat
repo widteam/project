daa7f4e9f4f2befe3f36f9de9fc6a69f8d7d06d5
Gyuri <sziklaygy@gmail.com>
2011-05-10 21:07:46 +0200

    /03_grafikus/ Teszteljétek! 03_grafikus.zip-ben is az van.

--- 03_grafikus/grafi_build.bat
+++ 03_grafikus/grafi_build.bat
@@ -7,22 +7,23 @@ rd /s /q dist
 echo Creating new directories...
 md build
 md build\classes
+md build\classes\images
 md dist
-md dist\images
 
 echo Updating the PATH...
-PATH=D:\Program Files\Java\jdk1.6.0_16\bin;
+PATH=C:\Program Files\Java\jdk1.6.0_23\bin;
 
 echo Copy the images...
-copy src\images\*.png dist\images
+copy src\images\*.png build\classes\images
+copy src\images\*.gif build\classes\images
 
 echo Creating the class files...
 javac -d build\classes src\*.java
 
 echo Creating the executable...
 cd build\classes
-echo Main-Class: main>manifest.mf
-jar mcf manifest.mf ..\..\dist\grafikus.jar *.class
+echo Main-Class: Main>manifest.mf
+jar mcf manifest.mf ..\..\dist\grafikus.jar *.class images\*.png images\*.gif
 del /q manifest.mf
 cd ..
 cd ..
