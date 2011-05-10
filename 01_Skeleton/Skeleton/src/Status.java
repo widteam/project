b4c3a92b535b41fe6972402e778810f98647aeda
tubeee <jeghtamas@gmail.com>
2011-05-10 13:21:04 +0200

    Szkeleton
    Signed-off-by: tubeee <jeghtamas@gmail.com>

---
+++ 01_Skeleton/Skeleton/src/Status.java
@@ -0,0 +1,14 @@
+/*
+* Nev: 			Status
+* Tipus: 		Enum
+* Interfacek:	---
+* Szulok		---
+*  
+*********** Leiras **********
+* A program futasi allapotai
+
+*/
+
+public enum Status {
+	RUNNING,STOPPED,PAUSED
+}
